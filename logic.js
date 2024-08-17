286c9608c5f8c03d977ca31978568cdaa4864781
Mathye-Simon <mathye.tsakane.simon@gmail.com>
2024-08-17 18:54:10 +0200

    modified the way user inputs grid size and added a function for that

--- logic.js
+++ logic.js
@@ -1,18 +1,40 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const container = document.getElementById('grid-container');
-    
-    // Create 16 rows
-    for (let i = 0; i < 16; i++) {
-        const gridRow = document.createElement('div');
-        gridRow.className = 'gridRow';
-        
-        // Create 16 cells in each row
-        for (let j = 0; j < 16; j++) {
-            const gridCell = document.createElement('div');
-            gridCell.className = 'grid-cell';
-            gridRow.appendChild(gridCell);
+const gridSize = prompt('Enter Grid Size: ')
+const container = document.querySelector('#grid-container')
+
+// create grid based on input
+function createGrid (gridSize) {
+    // nothing above 100!
+    if (gridSize > 100) {
+        gridSize = 100
+    }
+    // create rows
+    for (let i = 1; i <= gridSize; i++) {
+        const gridRow = document.createElement('div')
+        gridRow.className= 'gridRow'
+        container.appendChild(gridRow)
+
+        // cells
+        for (let j = 1; j<= gridSize; j++) {
+            const gridCell = document.createElement('grid')
+            gridCell.className = 'gridCell'
+            gridRow.appendChild(gridCell)
         }
         
-        container.appendChild(gridRow);
+
     }
-});
+
+    const cells = document.querySelectorAll('.gridCell')
+    cells.forEach(cell => {
+        cell.addEventListener('mouseover', () => {
+        cell.style.backgroundColor = 'red'
+        })
+    })
+        
+
+}
+
+
+createGrid(gridSize)
+alert('try different sizes!')
+
+
