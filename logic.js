9539758d2436dff61bab9ff539218a240e04da86
Mathye-Simon <mathye.tsakane.simon@gmail.com>
2024-08-18 22:48:12 +0200

    added new way of chosing color and changing grid size

--- logic.js
+++ logic.js
@@ -1,15 +1,26 @@
-const gridSize = prompt('Enter Grid Size: ')
+let changeGridSize = document.querySelector('#gridSize')
+let gridSize = parseInt(prompt('Please enter a number between 2 - 100'))
+const eraser = document.querySelector('#Eraser')
 const container = document.querySelector('#grid-container')
-const changebgColor = document.querySelector('#changebgColor')
+const selectColor = document.querySelector('.selectColor')
+let selectedColor = selectColor.value
+
+// erasing and color picking
+selectColor.addEventListener('click', () => {selectedColor = selectColor.value})   
+eraser.addEventListener('click', ()=> {
+    selectedColor='white'
+})
+
 const removeOutline = document.querySelector('#removeOutline')
 
 
 // create grid based on input
 function createGrid (gridSize) {
     // nothing above 100!
-    if (gridSize > 100) {
-        gridSize = 100
+    if (gridSize > 100 || gridSize < 2 || Number.isInteger(gridSize) == false ) {
+        gridSize = 35
     }
+
     // create rows
     for (let i = 1; i <= gridSize; i++) {
         const gridRow = document.createElement('div')
@@ -27,17 +38,15 @@ function createGrid (gridSize) {
     }
     let isDrawing = false
     const cells = document.querySelectorAll('.gridCell')
-    
     cells.forEach(cell => {
-        changebgColor.addEventListener('click', () => {cell.style.backgroundColor= 'black'})
         cell.addEventListener('mousedown', () => {
-            isDrawing = true 
-            cell.style.backgroundColor = 'red'
+            isDrawing = true
+            cell.style.backgroundColor = selectedColor
             })
 
         cell.addEventListener('mouseover', () => {
             if (isDrawing) {
-                cell.style.backgroundColor
+                cell.style.backgroundColor = selectedColor
             }
         })
         
@@ -47,16 +56,14 @@ function createGrid (gridSize) {
 
         cell.addEventListener('mouseleave', () => {
             if (isDrawing) {
-                cell.style.backgroundColor = 'red';
+                cell.style.backgroundColor = selectedColor;
             }
         })
     })
-        
-
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-    createGrid(gridSize);
+changeGridSize.addEventListener('click', ()=> {
+    location.reload()
 })
-
+document.addEventListener('DOMContentLoad',createGrid(gridSize))
 
